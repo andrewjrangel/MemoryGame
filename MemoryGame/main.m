4045c9275ffe55dca817d98c604e4a40c50f56d9
Andrew Rangel <andrewjrangel@gmail.com>
2013-04-17 19:06:50 -0500

    Initial Commit

---
+++ MemoryGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MemoryGame
+//
+//  Created by Ventura Rangel on 4/17/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
